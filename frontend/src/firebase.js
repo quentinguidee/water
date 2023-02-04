2883d8f6933fc1bb6fd0291907eaad6113efc623
Beddouch <78218394+SaraBedd@users.noreply.github.com>
2023-02-04 18:36:55 -0500

    firebase

---
+++ frontend/src/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://support.google.com/firebase/answer/7015592
+const firebaseConfig = {
+    apiKey: "AIzaSyBqSybCaKMe1urPNS0RHCZY-IwyZEXBjeE",
+    authDomain: "water-a278f.firebaseapp.com",
+    databaseURL: "https://water-a278f-default-rtdb.firebaseio.com",
+    projectId: "water-a278f",
+    storageBucket: "water-a278f.appspot.com",
+    messagingSenderId: "189403777883",
+    appId: "1:189403777883:web:ed5afa38ac227faecfa87e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,13 +3,28 @@ import * as React from 'react';
 import ConsumptionVisualizer from '../../components/ConsumptionVisualizer/ConsumptionVisualizer';
 import styles from './Home.module.sass';
 import InputWater from "../../components/InputWater/InputWater";
+import {addDoc, collection} from "firebase/firestore"
+import {db} from "../../firebase";
 import {useState} from "react";
+
 
 function Home() {
     const[waterLevel,setWaterLevel] = useState(0)
-    const onAdd = (value) => {
+
+    const onAdd = async (value) => {
         setWaterLevel(previous => previous + Number.parseInt(value));
+
+        try {
+            const docRef = await addDoc(collection(db, "waterIntake"), {
+                date: Date.now(),
+                amount: value
+            });
+            console.log("Document written with ID: ", docRef.id);
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
     }
+
     return (
         <div className={styles.home}>
             <div className={styles.left}>
