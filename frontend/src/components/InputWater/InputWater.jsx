7ed557840cef9612dd75eb6112dcb97ac7e3fa5f
Quentin Guidée <quentin.guidee@gmail.com>
2023-02-04 15:35:09 -0500

    [frontend] Add different quantities

--- frontend/src/components/InputWater/InputWater.jsx
+++ frontend/src/components/InputWater/InputWater.jsx
@@ -3,12 +3,42 @@ import * as React from "react";
 import styles from "./InputWater.module.sass";
 
 function InputWater() {
+    const capacities = [
+        {
+            value: 20,
+            symbol: "water_loss",
+        },
+        {
+            value: 30,
+            symbol: "water_medium",
+        },
+        {
+            value: 50,
+            symbol: "water_full",
+        }
+    ];
+
     return (
-        <div className={styles.wrapper}>
-            <input className={styles.input} type="number" placeholder="0"/>
-            <button className={styles.button}>
-                <span className="material-symbols-outlined">add</span>
-            </button>
+        <div className={styles.panel}>
+            <div className={styles.buttons}>
+                {capacities.map((capacity) => (
+                    <div className={styles.capacity}>
+                        <span className={"material-symbols-rounded"}>
+                            {capacity.symbol}
+                        </span>
+                        {capacity.value}ml
+                    </div>
+                ))}
+            </div>
+
+            <div>or</div>
+
+            <div className={styles.wrapper}>
+                <input className={styles.input} type="number" placeholder="0"/>
+                <button className={styles.button}>
+                    <span className="material-symbols-rounded">add</span>
+                </button>
+            </div>
         </div>
     );
 }
